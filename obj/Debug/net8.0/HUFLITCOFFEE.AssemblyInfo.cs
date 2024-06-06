9e4ba81cbfe78483aa34b17099b0d57d60ef1ba9
wuan204 <144576545+wuan204@users.noreply.github.com>
2024-06-07 00:15:21 +0700

    kádks

--- obj/Debug/net8.0/HUFLITCOFFEE.AssemblyInfo.cs
+++ obj/Debug/net8.0/HUFLITCOFFEE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HUFLITCOFFEE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fc2c947ea1fa02e8e672c6d06ca5e279d33f9bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f622ff68b774567ad5cd3b90df11c718a0fa8764")]
 [assembly: System.Reflection.AssemblyProductAttribute("HUFLITCOFFEE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HUFLITCOFFEE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
